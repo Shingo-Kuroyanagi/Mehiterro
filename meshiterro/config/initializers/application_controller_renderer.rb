13528fc627688c97f281283dd5302ae64e6a6811
Shingo-Kuroyanagi <ec2-user@ip-172-31-2-244.ec2.internal>
2020-09-11 16:10:45 +0000

    [Finish]4-3

--- meshiterro/config/initializers/application_controller_renderer.rb
+++ meshiterro/config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = '1a573ceff0fb58ef499fc0c358eba7b61670c7ccfa4d914e5fc93375a477ac3d04b2bbddcd91db72ab3ae282287f262dc5c25a793a833ece5fefcf76d445abfb'
