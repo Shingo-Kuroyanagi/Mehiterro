eb86cd00a525c034e9c1dc73cb8611e619ad3fb2
Shingo-Kuroyanagi <ec2-user@ip-172-31-2-244.ec2.internal>
2020-09-13 09:49:29 +0000

    [Finish]4-4

--- meshiterro/config/routes.rb
+++ meshiterro/config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+get 'users/show'
 root 'post_images#index'
 devise_for :users
-resources :post_images, only: [:new, :create, :index, :show] do
+resources :post_images, only: [:new, :create, :index, :show, :destroy] do
+    resource :favorites, only: [:create, :destroy]
     resources :post_comments, only: [:create, :destroy]
   end
+  resources :users, only: [:show]
 end
