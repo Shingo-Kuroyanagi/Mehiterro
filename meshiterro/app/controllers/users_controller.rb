4a8344752549a9d98636edab0ca6c2cdebfed1ed
Shingo-Kuroyanagi <ec2-user@ip-172-31-2-244.ec2.internal>
2020-09-14 04:49:41 +0000

    [Finish]4-6

--- meshiterro/app/controllers/users_controller.rb
+++ meshiterro/app/controllers/users_controller.rb
@@ -7,3 +7,13 @@ end
  def edit 
    @user = User.find(params[:id])
  end
+ def update
+     @user = User.find(params[:id])
+     @user.update
+     redirect_to user_path(@user.id)
+ end
+ private 
+  def user_params
+   params.require(:user).permit(:name, :profile_image)
+  end
+   
